8cbe77fb62a5f8e81c7e50350c74623d0bdf8fca
jeenson <jeensonaguilar84@gmail.com>
2017-11-27 22:16:35 -0500

    Ajustanto los middleware static

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,16 +1,17 @@
 var express = require('express')
 var app = express()
- 
+
+/*
+Cuando se desea usar carpeta estatica o definidas para ciertos propositos
+app.use("/web", express.static('public'));
+*/
+app.use(express.static('public'));
+
 app.set("view engine", "jade");
 
 app.get('/', function (req, res) {
   res.render("index");
 })
-
-app.get('/', function (req, res) {
-  res.render("index");
-})
-
 
 app.get('/login', function (req, res) {
 	res.render("login");
